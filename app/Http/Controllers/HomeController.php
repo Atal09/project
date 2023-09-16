68d21b6ea2de8b915aba80fcf9ba361278ebd577
Atal09 <113996359+Atal09@users.noreply.github.com>
2023-09-16 19:34:52 +0200

    test

---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+class HomeController extends Controller
+{
+    public function index()
+    {
+        return view('home');
+        
+        $welkomstTekst = "Welkom op onze website!";
+        return view('home', compact('welkomstTekst'));
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,11 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+
+use App\Http\Controllers\HomeController;
+Route::get('/home', [RandomController::class, 'index']);
+
+
+Route::get('home/{naam}',[RandomController::class,'index']);
